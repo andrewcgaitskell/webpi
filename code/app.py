8686575bcc9ceaa5daec66deb0f137fc17d2d766
andrewcgaitskell <andrew@gaitskell.com>
2017-12-24 18:20:25 +0000

    Update app.py

--- code/app.py
+++ code/app.py
@@ -12,7 +12,8 @@ def say_hi(name):
 @app.route('/')
 def index():
    link1 = '<p><a href="https://www.w3schools.com/html/">Visit our HTML tutorial</a></p>'
-   return '<html><body><h1>Hello World</h1>' + link1 + '</body></html>'
+   redtree = '<p><a href="http://192.168.1.11/pattern/red">Make Tree Red</a></p>'
+   return '<html><body><h1>Hello World</h1>' + redtree + '</body></html>'
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=True)
