d3671dd3e53b57460efd8a4ed91cedaa42bb58d9
andrewcgaitskell <andrew@gaitskell.com>
2018-12-15 12:47:39 +0000

    Update __init__.py

--- webpi/__init__.py
+++ webpi/__init__.py
@@ -1,7 +1,7 @@
 from flask import render_template
 
-@app.route('/hello/')
-@app.route('/hello/<name>')
+@webpi.route('/hello/')
+@webpi.route('/hello/<name>')
 def hello(name=None):
     return render_template('hello.html', name=name)
 
