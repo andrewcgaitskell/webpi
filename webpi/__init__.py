53dfbf8abb100416322f74161f7b56c6dc545fa1
andrewcgaitskell <andrew@gaitskell.com>
2018-12-15 12:25:56 +0000

    Update __init__.py

--- webpi/__init__.py
+++ webpi/__init__.py
@@ -1,1 +1,12 @@
-from app import Temp
+from flask import render_template
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
+
+class Temp:
+    def __init__(self):
+        ''' Constructor for this class. '''
+        # Create some member animals
+        self.members = ['Tiger', 'Elephant', 'Wild Cat']
