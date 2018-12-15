fcb537f03aa17b981905fbdc2fa354365a7e6c47
andrewcgaitskell <andrew@gaitskell.com>
2018-12-15 09:02:28 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
+from flask import render_template
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
+
 class Temp:
     def __init__(self):
         ''' Constructor for this class. '''
