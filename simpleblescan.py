ad9533cbde479be9e3c411352df7f55428cea84a
andrewcgaitskell <andrew@gaitskell.com>
2018-12-22 18:21:15 +0000

    Update simpleblescan.py

--- simpleblescan.py
+++ simpleblescan.py
@@ -6,3 +6,9 @@ dev = btle.Peripheral("98:4F:EE:0C:FB:00")
 print("Services...")
 for svc in dev.services:
   print(str(svc))
+  
+led3 = btle.UUID("acf1399a-05de-4f19-932d-9b82e23c78bc")
+ 
+ledService = dev.getServiceByUUID(led3)
+for ch in ledService.getCharacteristics():
+  print str(ch)
