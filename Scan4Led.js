03def5d95f9fb90c1b444aa9a476346dfb6dee4b
andrewcgaitskell <andrew@gaitskell.com>
2018-12-23 20:17:22 +0000

    Update Scan4Led.js

--- Scan4Led.js
+++ Scan4Led.js
@@ -22,7 +22,7 @@ Modified from Maria Paula Saba's exploreExample.
 var noble = require('noble');   //noble library
 var peripheralName = "LED";     // the local name of the peripheral you want
 var targetService = 'acf1399a05de4f19932d9b82e23c78bc';         // the service you want
-var targetCharacteristic = 'b3097f19d2c44a03951ea95ccd203a3b';  // the characteristic you want
+var targetCharacteristic = '1561ec697ead48f69b0b6a66c4c2493e';  // the characteristic you want
 
 // The scanning function
 function scan(state){
@@ -52,7 +52,7 @@ function findMe (peripheral) {
     // you could also use peripheral.discoverSomeServicesAndCharacteristics here,
     // and filter by the target service and characteristic:
     var serviceUUIDs = ["acf1399a05de4f19932d9b82e23c78bc"];
-    var characteristicUUIDs = ["b3097f19d2c44a03951ea95ccd203a3b"];
+    var characteristicUUIDs = ["1561ec697ead48f69b0b6a66c4c2493e"];
     peripheral.discoverSomeServicesAndCharacteristics(serviceUUIDs, characteristicUUIDs, [callback(error, services, characteristics));    peripheral.discoverAllServicesAndCharacteristics(exploreMe);
   }
 
