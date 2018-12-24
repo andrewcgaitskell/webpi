11413f4d1c757c6df1585cfd182198bacf451191
andrewcgaitskell <andrew@gaitskell.com>
2018-12-24 16:55:09 +0000

    Update listener.js

--- myapp/homeapp/listener.js
+++ myapp/homeapp/listener.js
@@ -48,12 +48,7 @@ const client = new Client({
   max: 10, // max number of clients in the pool
   idleTimeoutMillis: 30000 // how long a client is allowed to remain idle before being closed
 })
-client.connect()
 
-client.query('SELECT NOW()', (err, res) => {
-  console.log(err, res)
-  client.end()
-})
 
 client.connect(function(err, client) {
   if(err) {
