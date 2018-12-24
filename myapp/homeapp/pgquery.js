9d6c9c080d8e5bb48fd2dcd8f29d8cef7c6b8f66
andrewcgaitskell <andrew@gaitskell.com>
2018-12-24 11:56:50 +0000

    Update pgquery.js

--- myapp/homeapp/pgquery.js
+++ myapp/homeapp/pgquery.js
@@ -2,13 +2,19 @@ const express = require('express')
 const app = express()
 var pg = require('pg')
 var format = require('pg-format')
+var PGHOST='localhost'
 var PGUSER = 'pi'
 var PGDATABASE = 'data'
+var PGPASSWORD = 'raspberry'
+var listenport = 3000
 var age = 732
 
 var config = {
   user: PGUSER, // name of the user account
+  host: PGHOST,
   database: PGDATABASE, // name of the database
+  password: PGPASSWORD,
+  //port: listenport,
   max: 10, // max number of clients in the pool
   idleTimeoutMillis: 30000 // how long a client is allowed to remain idle before being closed
 }
