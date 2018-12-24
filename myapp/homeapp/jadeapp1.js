114f974841eaa11ee5e333f61b8dc993d0404193
andrewcgaitskell <andrew@gaitskell.com>
2018-12-24 22:00:53 +0000

    Update jadeapp1.js

--- myapp/homeapp/jadeapp1.js
+++ myapp/homeapp/jadeapp1.js
@@ -8,15 +8,6 @@ var express = require('express')
 
 var app = express()
 
-const { Pool, Client } = require('pg')
-
-const pool = new Pool({
-  user: 'pi',
-  host: 'localhost',
-  database: 'data',
-  password: 'raspberry',
-  port: 5432,
-})
 function compile(str, path) {
   return stylus(str)
     .set('filename', path)
@@ -33,49 +24,5 @@ app.use(stylus.middleware(
 ))
 
 app.use(express.static(__dirname + '/public'))
-
-app.get('/', function (req, res) {
-
-const client = new Client({
-  user: 'pi',
-  host: 'localhost',
-  database: 'data',
-  password: 'raspberry',
-  port: 5432,
-})
-
-client.connect()
-
-client.query('SELECT id, description, device, service, characteristic, value, "timestamp" FROM lights.state',
-             (err, res) => {
-  console.log(err, res)
-  client.end()
-})
-  
-    var sql = require("mssql");
-
-    var config = {
-        user: 'sa',
-        password: 'sjkaria',
-        server: 'localhost',
-        database: 'SchoolDB' 
-    };
-
-    sql.connect(config, function (err) {
-        
-        if (err) console.log(err);
-
-        var request = new sql.Request();
-
-        request.query('', function (err, recordset) {
-            
-            if (err) 
-                console.log(err)
-            else
-                res.render('StateList', { stateList: recordset });
-            
-        });
-    });
-});
 
 app.listen(3000)
