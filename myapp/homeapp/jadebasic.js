67525636d663e42b17d0b2789aa27bd07423ba62
andrewcgaitskell <andrew@gaitskell.com>
2018-12-24 22:32:44 +0000

    Update jadebasic.js

--- myapp/homeapp/jadebasic.js
+++ myapp/homeapp/jadebasic.js
@@ -27,4 +27,9 @@ app.use(express.static(__dirname + '/public'))
 app.get('/', function (req, res) {
   res.end('Hi there!')
 })
+
+app.get('/table', function (req, res) {
+  res.render('table')
+})
+
 app.listen(3000)
