828ff58f219941e4dc044935bc90e44524c81699
andrewcgaitskell <andrew@gaitskell.com>
2018-12-26 10:52:57 +0000

    Create getvaluetable.js

---
+++ myapp/homeapp/public/getvaluetable.js
@@ -0,0 +1,61 @@
+const express = require('express')
+const app = express()
+const pg = require('pg')
+var format = require('pg-format')
+var PGHOST='localhost'
+var PGUSER = 'pi'
+var PGDATABASE = 'data'
+var PGPASSWORD = 'raspberry'
+var listenport = 3000
+var age = 732
+
+var config = {
+  user: PGUSER, // name of the user account
+  host: PGHOST,
+  database: PGDATABASE, // name of the database
+  password: PGPASSWORD,
+  //port: listenport,
+  max: 10, // max number of clients in the pool
+  idleTimeoutMillis: 30000 // how long a client is allowed to remain idle before being closed
+}
+
+//var pgConString = "postgres://localhost/bjorngylling"
+
+//const pgConString = 'postgresql://pi:raspberry@localhost:3000/data'
+const { Pool, Client } = require('pg')
+
+const pool = new Pool({
+  user: PGUSER, // name of the user account
+  host: PGHOST,
+  database: PGDATABASE, // name of the database
+  password: PGPASSWORD,
+  //port: listenport,
+  max: 10, // max number of clients in the pool
+  idleTimeoutMillis: 30000 // how long a client is allowed to remain idle before being closed
+})
+
+pool.query('SELECT NOW()', (err, res) => {
+  console.log(err, res)
+  pool.end()
+})
+
+const client = new Client({
+  user: PGUSER, // name of the user account
+  host: PGHOST,
+  database: PGDATABASE, // name of the database
+  password: PGPASSWORD,
+  //port: listenport,
+  max: 10, // max number of clients in the pool
+  idleTimeoutMillis: 30000 // how long a client is allowed to remain idle before being closed
+})
+
+
+client.connect(function(err, client) {
+  if(err) {
+    console.log(err);
+  }
+  client.on('notification', function(msg) {
+    console.log(msg);
+  });
+  var query = client.query("LISTEN watchers");
+});
