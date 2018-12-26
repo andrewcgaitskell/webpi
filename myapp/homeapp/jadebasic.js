b7d1cfb066f6407de1eb57c05609fb8e1646bfdc
andrewcgaitskell <andrew@gaitskell.com>
2018-12-26 11:23:29 +0000

    Update jadebasic.js

--- myapp/homeapp/jadebasic.js
+++ myapp/homeapp/jadebasic.js
@@ -6,6 +6,8 @@ var express = require('express')
   , nib = require('nib')
   , logger = require('morgan');
 
+var format = require('pg-format')
+
 var app = express()
 
 function compile(str, path) {
@@ -28,8 +30,54 @@ app.get('/', function (req, res) {
   res.end('Hi there!')
 })
 
+
+var PGHOST='localhost'
+var PGUSER = 'pi'
+var PGDATABASE = 'data'
+var PGPASSWORD = 'raspberry'
+var listenport = 3000
+//var age = 732
+
+var config = {
+  user: PGUSER, // name of the user account
+  host: PGHOST,
+  database: PGDATABASE, // name of the database
+  password: PGPASSWORD,
+  //port: listenport,
+  max: 10, // max number of clients in the pool
+  idleTimeoutMillis: 30000 // how long a client is allowed to remain idle before being closed
+}
+
+//var pgConString = "postgres://localhost/bjorngylling"
+
+//const pgConString = 'postgresql://pi:raspberry@localhost:3000/data'
+const { Pool, Client } = require('pg')
+
+const pool = new Pool({
+  user: PGUSER, // name of the user account
+  host: PGHOST,
+  database: PGDATABASE, // name of the database
+  password: PGPASSWORD,
+  //port: listenport,
+  max: 10, // max number of clients in the pool
+  idleTimeoutMillis: 30000 // how long a client is allowed to remain idle before being closed
+})
+
 app.get('/table', function (req, res) {
-  res.render('table')
+  const { Pool, Client } = require('pg')
+  const pool = new Pool({
+  user: PGUSER, // name of the user account
+  host: PGHOST,
+  database: PGDATABASE, // name of the database
+  password: PGPASSWORD,
+  //port: listenport,
+  max: 10, // max number of clients in the pool
+  idleTimeoutMillis: 30000 // how long a client is allowed to remain idle before being closed
+})
+  pool.query('SELECT id, description, device, service, characteristic, value, timestamp FROM lights.state', (err, res) => {
+  console.log(err, res)
+  res.render('ValuesTable', { title: 'Values Tables', rows: rows })
+  pool.end()
 })
 
 app.listen(3000)
