e22d2e99fc4635f4c9ef2a4ea54ee5527e6e8206
andrewcgaitskell <andrew@gaitskell.com>
2018-12-26 12:17:21 +0000

    Update pgquery.js

--- myapp/homeapp/pgquery.js
+++ myapp/homeapp/pgquery.js
@@ -3,20 +3,23 @@ const app = express()
 
 var pg = require('pg')
 var format = require('pg-format')
-var PGHOST='localhost'
-var PGUSER = 'pi'
+//var PGHOST='localhost'
+//var PGUSER = 'pi'
 var PGDATABASE = 'data'
-var PGPASSWORD = 'raspberry'
+//var PGPASSWORD = 'raspberry'
 var listenport = 3000
 var age = 732
 
 require('dotenv').config()
 
 var config = {
-  user: PGUSER, // name of the user account
-  host: PGHOST,
+  //user: PGUSER, // name of the user account
+  PGUSER: process.env.DB_USER,  
+  //host: PGHOST,
+  host: process.env.DB_HOST,
   database: PGDATABASE, // name of the database
-  password: PGPASSWORD,
+  //password: PGPASSWORD,
+  password: process.env.DB_PASS,  
   //port: listenport,
   max: 10, // max number of clients in the pool
   idleTimeoutMillis: 30000 // how long a client is allowed to remain idle before being closed
